4a1343206cf02a4ba9a0883a35447fe857cec7b9
Hussen Yesuf <124931614+HussenYesufAlli@users.noreply.github.com>
2023-06-17 11:27:54 +0800

    Update solution q2.m

--- q2.m
+++ q2.m
@@ -1,3 +1,7 @@
+%##########################################################
+%#####     Name: ALLI HUSSEN YESUF
+%#####     Student ID: 722106020008
+%##########################################################
 
 % Define the matrix B
 n = 4;
@@ -6,39 +10,39 @@ B = rand(n);
 % Set the initial vector
 u = ones(n, 1);
 
-% Set the stopping criterion parameters
+
+A = B + B.' % A = B + BTranspose
 max_iterations = 10;
 tolerance = 1e-6;
 
-% Power method iterations
-for k = 1:max_iterations
-    v = B * u;  % Calculate the product B * u
-    [~, index] = max(abs(v));  % Find the index of the maximum element in v
-    max_element = v(index);
-    v_normalized = v / max_element;  % Normalize v
-    
-    % Check stopping criterion
-    if norm(u - v_normalized) < tolerance
-        break;
+[eigen_value,eigen_vector]=invmi(A,u,tolerance,max_iterations)  % Resukts eigenvalue and eigenvector
+
+
+function [s,y]=invmi(A,u,e,N) 
+ k=1; r=0;  
+ y=u./max(abs(u));  
+ [L,U,P]=lu(A); % LU decomposition of A with pivoting
+ px = P*y; % Apply pivot
+ z=L\px; % solve via LU
+ x=U\z;
+ max_entry=max(x);% find the maximum entry in absolute value 
+ s=1/max_entry;         
+ if abs(max_entry-r)<eps  
+    return
+ end
+ while abs(max_entry-r)>=e && k<=N % stopping rule
+    if k==1
+    disp('The system has started')
     end
-    
-    u = v_normalized;  % Update u for the next iteration
+    r=max_entry;
+    y=x./max(abs(x));
+    px = P*y;
+    z=L\px;
+    x=U\z
+    max_entry = max(abs(x))  
+    fprintf('Just finished iteration #%d\n', k);
+    k=k+1;
+ end
+   [m,index]=max(abs(x)); 
+    s=1/x(index); 
 end
-
-% Compute the eigenvalue
-lambda = max_element;
-
-% Normalize the eigenvector
-eigenvector = u / norm(u);
-
-% Output the results
-iteration_number = k;
-disp('Iteration number:');
-disp(iteration_number);
-
-disp('Eigenvalue:');
-disp(lambda);
-
-disp('Eigenvector:');
-disp(eigenvector);
-
