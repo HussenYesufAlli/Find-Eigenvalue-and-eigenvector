88656dc6462e134352dd9f197061b91c92b13386
Hussen Yesuf <124931614+HussenYesufAlli@users.noreply.github.com>
2023-06-17 11:26:30 +0800

    Update solution q1.m

--- q1.m
+++ q1.m
@@ -1,18 +1,51 @@
-n = 5; % Size of the matrix
-A = zeros(n); % Initialize the matrix A
-b = zeros(n, 1); % Initialize the vector b
-x_exact = [2; -1; 2; -1; 2]; % Exact solution for comparison
+n = 5; % Number of unknowns
+A = zeros(n);
+b = zeros(n, 1);
 
-% Fill the matrix A and vector b
+% Constructing matrix A and vector b
 for i = 1:n
     for j = 1:n
-        A(i, j) = i*j + (-1)^(i+j);
+        A(i, j) = 1 / (i + j - 1);
     end
     b(i) = sum(A(i, :));
 end
 
-% Solve Ax = b using LU decomposition
-[L, U, P] = lu(A);
-y = L\(P*b);
-x = U\y;
+% Solving Ax = b using the direct method (backslash operator)
+x_direct = A \ b;
 
+% Solving Ax = b using the iterative method (Jacobi or Gauss-Seidel)
+x_iterative = ones(n, 1); % Initial guess for x
+tolerance = 1e-6; % Tolerance for convergence
+max_iterations = 1000; % Maximum number of iterations
+
+% Iterative method (e.g., Jacobi)
+for iteration = 1:max_iterations
+    x_previous = x_iterative;
+    for i = 1:n
+        x_iterative(i) = (b(i) - sum(A(i, :) * x_previous) + A(i, i) * x_previous(i)) / A(i, i);
+    end
+    if norm(x_iterative - x_previous, 2) < tolerance
+        break;
+    end
+end
+
+% Computing b - Ax and x - x_direct
+residual_direct = norm(b - A * x_direct, 2);
+residual_iterative = norm(b - A * x_iterative, 2);
+difference = norm(x_iterative - x_direct, 2);
+
+% Displaying the results
+disp("Direct method:");
+disp("x:");
+disp(x_direct');
+disp("b - Ax (2-norm):");
+disp(residual_direct);
+disp("x - x_direct (2-norm):");
+disp(difference);
+disp("Iterative method:");
+disp("x:");
+disp(x_iterative');
+disp("b - Ax (2-norm):");
+disp(residual_iterative);
+disp("x - x_direct (2-norm):");
+disp(difference);
